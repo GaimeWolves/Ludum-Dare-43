79de2ec9f54a885dc989914a9cf57e5b67798ac3
GameWolves <ndjerfi16@gmail.com>
2018-12-01 08:53:09 +0100

    Created the project

---
+++ Electrix/desktop/src/gamewolves/itch/io/electrix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gamewolves.itch.io.electrix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gamewolves.itch.io.electrix.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
